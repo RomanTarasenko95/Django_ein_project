5f745e89fe6b16423fdeb5dd0a93ae41bab51d4b
Romeo <pe4unis@gmail.com>
2024-07-30 10:20:04 +0200

    hello2

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
+    path('hello2/', views.hello2, name='hello2'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello, Romeo")
+
+
+def hello2(request):
+    return HttpResponse("Hello, Romeo2")
