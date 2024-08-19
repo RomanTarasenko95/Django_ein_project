3199d6e7f324945fa8ad988b7bdba157e33809a5
Romeo <pe4unis@gmail.com>
2024-08-19 10:49:00 +0200

    dz_11

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import TaskCreateView, TaskListView, TaskStatsView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('hello/', views.hello, name='hello'),
     path('bye/', views.bye, name='bey'),
+    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,45 @@
+from time import timezone
+
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Count, Q
+from .models import Task
+from django.utils import timezone
+
+
+class TaskStatsView(APIView):
+    def get(self, request):
+        total_tasks = Task.objects.count()
+        status_counts = Task.objects.values('status').annotate(count=Count('status'))
+        overdue_tasks = Task.objects.filter(deadline__lt=timezone.now()).count()
+
+        stats = {
+            'total_tasks': total_tasks,
+            'status_counts': status_counts,
+            'overdue_tasks': overdue_tasks,
+        }
+        return Response(stats)
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['status', 'deadline']
+    ordering_fields = ['deadline', 'status']
+    pagination_class = PageNumberPagination
+
+
+class TaskCreateView(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
 def hello(request):
